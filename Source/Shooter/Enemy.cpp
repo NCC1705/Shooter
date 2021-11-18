b8180863456e0d263f665b9e11d8c253028e0adf
Neamtu Costin Cristian <caostineu@yahoo.com>
2021-11-18 20:37:47 +0200

    Enemies

--- Source/Shooter/Enemy.cpp
+++ Source/Shooter/Enemy.cpp
@@ -99,8 +99,8 @@ void AEnemy::BeginPlay()
 
 	const FVector WorldPatrolPoint2 = UKismetMathLibrary::TransformLocation(GetActorTransform(), PatrolPoint2);
 
-	DrawDebugSphere(GetWorld(), WorldPatrolPoint, 25.f, 12, FColor::Red, true);
-	DrawDebugSphere(GetWorld(), WorldPatrolPoint2, 25.f, 12, FColor::Red, true);
+	//DrawDebugSphere(GetWorld(), WorldPatrolPoint, 25.f, 12, FColor::Red, true);
+	//DrawDebugSphere(GetWorld(), WorldPatrolPoint2, 25.f, 12, FColor::Red, true);
 
 	if (EnemyController)
 	{
